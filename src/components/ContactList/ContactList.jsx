82544b3886f65d57e1c51946623d18c0f795696a
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-03-28 14:22:35 +0300

    add toLowerCase() in filter

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,12 +1,17 @@
 import Contact from "components/Contact/Contact";
+import PropTypes from 'prop-types';
 
 const ContactList = ({contactsProp, removeContact}) => {
-    // console.log(contactsProp);
     return (
         <ul>
             {contactsProp.map(contact => <Contact key={contact.id} contactProp={contact} removeContact={removeContact}/>)}
         </ul>
     );
+};
+
+ContactList.propTypes = {
+    contactsProp: PropTypes.array.isRequired,
+    removeContact: PropTypes.func.isRequired
 };
 
 export default ContactList;
